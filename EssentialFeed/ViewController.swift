d3284da74e5b4f34694eaa75b1c1a13b4f5c2578
Scott Bauer <bauerscottt@gmail.com>
2021-12-17 14:39:03 -0500

    Initial Commit

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Scott Bauer on 12/17/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
